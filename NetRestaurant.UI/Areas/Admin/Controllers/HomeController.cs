085ec547247e84855ff171c29fd123cd2df3ae19
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2024-12-23 11:06:15 -0300

    Finish Lecture

--- NetRestaurant.UI/Areas/Admin/Controllers/HomeController.cs
+++ NetRestaurant.UI/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NetRestaurant.UI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("/admin/[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = "AdminCookie")]
     public class HomeController : Controller
     {
         public IActionResult Index()
