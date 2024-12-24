9bd2e0f252d1b9861dd21f2294eb496b54319e7a
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2024-12-24 16:41:39 -0300

    Finish Lecture

---
+++ NetRestaurant.UI/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetRestaurant.Infrastructure.Repositories;
+
+namespace NetRestaurant.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/admin/[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = "AdminCookie")]
+    public class UsersController : Controller
+    {
+        private readonly UserRepository _userRepository;
+
+        public UsersController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _userRepository.GetAll());
+        }
+    }
+}
